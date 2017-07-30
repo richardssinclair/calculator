e47c9127794687a80cbfb9a641b0829ee62ed554
richardssinclair <richardssinclair@gmail.com>
2017-07-30 22:40:49 +0100

    save function added

--- js/app.js
+++ js/app.js
@@ -6,6 +6,18 @@ var firstNumber;
 var operator;
 var secondNumber;
 
+var savedMaths;
+var answer
+
+
+//SAVE: Press to save your current math into Saved Maths. When pressed, the user will be
+// prompt to submit a name
+
+// Saved Maths: List of selectable maths. Maths should be ordered by date and time Math Name/Date: Press to display the selected math in Result
+
+// DELETE: Remove a saved Math Name/Date from Saved Maths
+
+// CANCEL: Press to reset Result
 
 
 //the function that makes the calculator work
@@ -17,6 +29,9 @@ function start(){
   //gets the equalsby  using the class name from html but starts allways with 0, so when the calculator starts it opens with 0
   var equals    = document.getElementsByClassName('equals')[0];
 
+  var save      = document.getElementsByClassName('save')[0];
+
+
   //for loop, runs through the html and goes through all the numbers adding event listeners
   for (var i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', setNumber);
@@ -24,9 +39,12 @@ function start(){
   // another for loop for the operators
   for (var j = 0; j < operators.length; j++) {
     operators[j].addEventListener('click', setOperator);
+    console.log(operator)
   }
 
   equals.addEventListener('click', calculate);
+
+  save.addEventListener('click', saveMaths);
 }
 
 function setNumber() {
@@ -45,12 +63,10 @@ function setNumber() {
 
 function setOperator(){
   operator = this.value;
-  // console.log(firstNumber, operator, secondNumber);
+  console.log(firstNumber, operator, secondNumber);
 }
 //this function performs the calculations, using a switch statement for the opperators and using the variables first and second number
 function calculate(){
-
-  var answer
 
   switch (operator) {
     case '+':
@@ -65,11 +81,9 @@ function calculate(){
     case '/':
       answer = firstNumber / secondNumber
       break
-      case '.':
-        answer = firstNumber.secondNumber
-        break
     default:
       break;
+      console.log(operator)
   };
   updateDisplay(answer);
   // console.log(operator);
@@ -78,4 +92,14 @@ function calculate(){
 function updateDisplay(value){
   var display = document.getElementsByClassName('display')[0];
   display.value = value;
+}
+
+
+function saveMaths(){
+  var name = prompt("Enter a name for this calculation");
+  var list = document.getElementById('savedItems');
+  var entry = document.createElement('li');
+  entry.appendChild(document.createTextNode(name + '-' + answer));
+  list.appendChild(entry);
+
 }
